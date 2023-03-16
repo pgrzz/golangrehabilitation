45b2a4e9cce3e60930b103177f49c2d9257be28c
pppgr <pgrpgrpgr3@gail.com>
2023-03-16 16:22:34 +0800

    第k大的数字

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"golangrehabilitation/mypackage"
 )
 
@@ -14,17 +13,14 @@ func main() {
 	// 	{15, 14, 12, 16},
 	// }
 
-	matrix := [][]int{
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9},
-	}
+	// matrix := [][]int{
+	// 	{1, 2, 3},
+	// 	{4, 5, 6},
+	// 	{7, 8, 9},
+	// }
 
-	mypackage.Rotate(matrix)
+	arrays := []int{3, 2, 3, 1, 2, 4, 5, 5, 6}
 
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix); j++ {
-			fmt.Println(matrix[i][j])
-		}
-	}
+	mypackage.FindKthLargest(arrays, 4)
+
 }
---
+++ mypackage/kth.go
@@ -0,0 +1,68 @@
+package mypackage
+
+func FindKthLargest(nums []int, k int) int {
+
+	// 构建最大堆	[2n+1],[2n+2]
+	length := len(nums)
+	for i := length/2 - 1; i >= 0; i-- {
+		compareAndSwap(nums, i, length)
+	}
+	//remove 前k个 通过伸缩size来非显式的的删除，然后每次重新维护一次堆性质
+	size := length
+	for i := 1; i < k; i++ {
+		swap(nums, 0, size-1)
+		//resort
+		size--
+		compareAndSwap(nums, 0, size)
+
+	}
+	return nums[0]
+}
+
+// 默认的肯定是左边会比右边大 构建堆的过程是 从n/2的节点开始判断每一个叶子节点最大值做交换，递归到底层
+func compareAndSwap(nums []int, i int, length int) {
+
+	largest := nums[i]
+	side := 0
+
+	subNode := 2*i + 1
+	if subNode < length {
+		if nums[subNode] > largest {
+			side = 1
+			largest = nums[subNode]
+		}
+	}
+	subNode = 2*i + 2
+	if subNode < length {
+		if side == 1 {
+			//判断是否有另外一个节点值大
+			if nums[subNode] > largest {
+				side = 2
+				largest = nums[subNode]
+			}
+		} else {
+			if nums[subNode] > nums[i] {
+				side = 2
+
+			}
+		}
+	}
+	//判断side
+	if side == 1 {
+		subNode = 2*i + 1
+	} else if side == 2 {
+		subNode = 2*i + 2
+	}
+	if side != 0 {
+		swap(nums, i, subNode)
+		//在存在和右节点交换的场景,检查现在的subNode和另一个节点谁大，大的应该在左边
+		compareAndSwap(nums, subNode, length)
+	}
+
+}
+
+func swap(nums []int, i int, j int) {
+	temp := nums[i]
+	nums[i] = nums[j]
+	nums[j] = temp
+}
