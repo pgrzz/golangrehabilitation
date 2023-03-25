65e215eeea2a6d5b06c6bb4750a5edf322caec1f
pppgr <pgrpgrpgr3@gail.com>
2023-03-25 15:48:03 +0800

    区间合并

--- mypackage/array.go
+++ mypackage/array.go
@@ -77,3 +77,86 @@ func merge(intervals [][]int) [][]int {
 	return result
 
 }
+
+// 1、判断 i中2, i+1中的1 是否有交集
+// https://leetcode.cn/problems/merge-intervals/
+func Merge22(intervals [][]int) [][]int {
+	n := len(intervals)
+
+	if n <= 1 {
+		return intervals
+	}
+
+	result := [][]int{}
+
+	i := 0
+	j := 1
+
+	//sort一把 直接插入排序好了
+	for t := 1; t < len(intervals); t++ {
+		tempArray := intervals[t]
+		q := t - 1
+		for q >= 0 && tempArray[0] < intervals[q][0] {
+			intervals[q+1] = intervals[q]
+			q--
+		}
+		intervals[q+1] = tempArray
+	}
+
+	for i < n && j < n {
+		array1 := intervals[i]
+		array2 := intervals[j]
+		//有交集则做合并，无交集时则加入到结果集合
+		//三种相交情况都需要做判断
+		//case1、 右交 array1[1]>array2[0] case 2、全包裹  array1[0]<array2[0] && array1[1]<array2[1]
+		// case3 左交 array1[0]>array2[1]
+		//同时镜像case
+		if (array2[0] <= array1[1] && array2[1] >= array1[0]) ||
+			(array1[0] <= array2[1] && array1[1] >= array2[0]) {
+			//2、缩小数组长度
+			//copy 就好了 i不动，j+1
+			maxa1 := Max(array1[1], array2[1])
+			array1[1] = maxa1
+
+			mina1 := Min(array1[0], array2[0])
+			array1[0] = mina1
+
+			//当 i+1 ==lens 如果合并完说明没有继续的了直接apend
+			if j == n-1 {
+				result = append(result, array1)
+			}
+
+			//让j向后移动，说明i已经合并了
+			j++
+		} else {
+			if j == n-1 {
+				//说明后面两个都需要合并
+				result = append(result, array1)
+				result = append(result, array2)
+			} else {
+				result = append(result, array1)
+			}
+			//i的对应数组 加入到结果中
+
+			i = j
+			j++
+		}
+
+	}
+	return result
+
+}
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
